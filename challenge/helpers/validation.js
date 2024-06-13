516ef2bbe3acc87d007617877fa9fe2ea2699d44
Carlos Azpurua <carlosazpurua1@gmail.com>
2024-06-13 12:09:54 -0300

    feat: add comments details to validation functions

--- challenge/helpers/validation.js
+++ challenge/helpers/validation.js
@@ -1,12 +1,30 @@
-
+/**
+ * Checks if a value is an array
+ * 
+ * @param {*} value 
+ * @returns {boolean} true if the value is an array, otherwise false
+ */
 export function isArray(value) {
   return Array.isArray(value);
 }
 
+/**
+ * Checks if a value is a string
+ * 
+ * @param {*} value 
+ * @returns {boolean} true if the value is a string, otherwise false
+ */
 export function isString(value) {
   return typeof value === 'string' || value instanceof String;
 }
 
+/**
+ * Throws an error if the data type of a value does not match the expected type
+ * 
+ * @param {*} value The value to check
+ * @param {Function} expectedType The function representing the expected type
+ * @throws {Error} If the data type of 'value' does not match 'expectedType'
+ */
 export function throwErrorIfInvalidType(value, expectedType) {
   if (!expectedType(value)) {
     throw new Error(`Expected type ${expectedType}, but received ${typeof value}`);
