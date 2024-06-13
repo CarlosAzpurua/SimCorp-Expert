a2510c6c34516c6c8cecaea59a74384f4e26b879
Carlos Azpurua <carlosazpurua1@gmail.com>
2024-06-12 21:12:32 -0300

    feat: add function longestSubstring

--- challenge/exercise.3.js
+++ challenge/exercise.3.js
@@ -16,13 +16,40 @@ Bonus: Can you solve this problem in linear time?
 
 */
 
-function longestSubstring(str){
-	//you can show the result as you would like
+/**
+ * This function find the longest substring in a string with no repeated characters.
+ *
+ * @param {string} str 
+ * @return {string} 
+ */
+function longestSubstring(str) {
+	if (str.length === 0) return "";
+  if (str.length === 1) return str;
+
+  let maxLength = 0;
+  let start = 0;
+  let seen = new Map();
+  let maxSubstring = "";
+
+  for (let end = 0; end < str.length; end++) {
+    if (seen.has(str[end])) {
+			//Math.max to be sure to advance/move the starting index only if is necessary
+      start = Math.max(seen.get(str[end]) + 1, start);
+    }
+    seen.set(str[end], end);
+    if (end - start + 1 > maxLength) {
+      maxLength = end - start + 1;
+      maxSubstring = str.slice(start, end + 1);
+    }
+  }
+
+  return maxSubstring;
 }
 
-longestSubstring("aaaaaa");
-longestSubstring("abcde");
-longestSubstring("abcda");
-longestSubstring("abcda");
+console.log(
+	longestSubstring("aaaaaa"),
+	longestSubstring("abcde"),
+	longestSubstring("abcda"),
+	longestSubstring("abcdefgabcbb"),
+)
 
-export default longestSubstring;
